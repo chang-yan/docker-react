0e783cc0bf5dc59ea7fafe0e8840fc9fe9320574
jasonwu <jasonwu@jubo.health>
2024-08-11 15:56:52 +0800

    feat: add a simple react app

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /Hi There! I am a React App running on Docker!/i,
+  );
+
   expect(linkElement).toBeInTheDocument();
 });
